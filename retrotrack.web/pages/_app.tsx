e2460ffceeb3295e99078bb1e5ca959376d6a02c
Benjamin Regan <35966588+Bregann@users.noreply.github.com>
2022-12-11 20:58:50 +0000

    metadata stuff

--- retrotrack.web/pages/_app.tsx
+++ retrotrack.web/pages/_app.tsx
@@ -20,6 +20,13 @@ export default function App(props: AppProps) {
           <title>RetroTrack</title>
           <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
           <link rel="icon" href="/favicon.ico" />
+          <meta property="og:title" content="RetroTrack" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://retrotrack.bregan.me" />
+          <meta property="og:image" content="/rt.png" />
+          <meta property="og:description" content="Achievement tracking website for RetroAchievements" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="description" content="RetroTrack is a Tracker for the website RetroAchievements. This Retro Achievement tracking site allows for you to track games, see your inprogress games, see newly added games plus more. New RetroAchievement consoles are added quickly and the site is updated daily." />
         </Head>
 
         <MantineProvider
