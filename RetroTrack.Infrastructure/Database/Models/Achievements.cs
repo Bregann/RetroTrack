64157185f68364443eb415b772d7dbede6fe9e80
Benjamin Regan <35966588+Bregann@users.noreply.github.com>
2023-06-19 22:05:58 +0100

    Add achievement icon to db

--- RetroTrack.Infrastructure/Database/Models/Achievements.cs
+++ RetroTrack.Infrastructure/Database/Models/Achievements.cs
@@ -11,6 +11,7 @@ namespace RetroTrack.Infrastructure.Database.Models
         public required long Id { get; set; }
         public required string AchievementName { get; set; }
         public required string AchievementDescription { get; set; }
+        public required string AchievementIcon { get; set; }
         public required int Points { get; set; }
         public required Games Game { get; set; }
     }
