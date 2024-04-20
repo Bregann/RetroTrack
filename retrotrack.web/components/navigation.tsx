0582c9c11e9ad7a20fa8f5c019793ac6e0d61b89
Benjamin Regan <35966588+Bregann@users.noreply.github.com>
2024-04-20 19:31:58 +0100

    navigation optimisation

--- retrotrack.web/components/navigation.tsx
+++ retrotrack.web/components/navigation.tsx
@@ -1,7 +1,7 @@
 import fetchHelper from '@/helpers/FetchHelper'
 import sessionHelper from '@/helpers/sessionHelper'
 import { type GetPublicNavigationDataDto } from '@/pages/api/navigation/GetPublicNavigationData'
-import { AppShell, Burger, Group, NavLink, ScrollArea } from '@mantine/core'
+import { AppShell, Burger, Button, Grid, Group, NavLink, ScrollArea } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
 import { IconHome2 } from '@tabler/icons-react'
 import { type AppProps } from 'next/app'
@@ -11,9 +11,22 @@ import { useEffect, useState } from 'react'
 const Navigation = (props: AppProps): JSX.Element => {
   const { Component, pageProps } = props
   const [opened, { toggle }] = useDisclosure()
+  const [loginModalOpened, setLoginModalOpened] = useState(false)
+  const [registerModalOpened, setRegisterModalOpened] = useState(false)
+  const [supportModalOpened, setSupportModalOpened] = useState(false)
   const [loggedIn, setLoggedIn] = useState(sessionHelper.hasSession())
   const [navData, setNavData] = useState<GetPublicNavigationDataDto[] | null>(null)
   const [activePage, setActivePage] = useState('')
+
+  const consoleTypes = [
+    { consoleType: 0, consoleName: 'Nintendo' },
+    { consoleType: 1, consoleName: 'Sony' },
+    { consoleType: 2, consoleName: 'Atari' },
+    { consoleType: 3, consoleName: 'Sega' },
+    { consoleType: 4, consoleName: 'NEC' },
+    { consoleType: 5, consoleName: 'SNK' },
+    { consoleType: 6, consoleName: 'Other' }
+  ]
 
   useEffect(() => {
     if (loggedIn) {
@@ -44,8 +57,46 @@ const Navigation = (props: AppProps): JSX.Element => {
         }}
         padding="md">
         <AppShell.Header>
-          <Group h="100%" px="md">
-            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <Group h="100%" px="md" justify="flex-end">
+            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" style={{ marginRight: 'auto' }} />
+            <Button
+              variant="gradient"
+              gradient={{ from: 'indigo', to: 'cyan' }}
+              onClick={() => { setSupportModalOpened(true) }}>
+              Support
+            </Button>
+            {loggedIn
+              ? <>
+                <Button
+                  variant="gradient"
+                  gradient={{ from: 'indigo', to: 'cyan' }}
+                  onClick={() => { /* TODO: Implement */ }}>
+                  Update games
+                </Button>
+
+                <Button
+                  variant="gradient"
+                  gradient={{ from: 'orange', to: 'red' }}
+                  onClick={() => { /* TODO: Implement - router.push them to homepage */ } }>
+                  Logout
+                </Button>
+              </>
+              : <>
+                <Button
+                  variant="gradient"
+                  gradient={{ from: 'indigo', to: 'cyan' }}
+                  onClick={() => { setLoginModalOpened(true) }}>
+                  Login
+                </Button>
+                <Button
+                  variant="gradient"
+                  gradient={{ from: 'teal', to: 'lime', deg: 105 }}
+                  onClick={() => { setRegisterModalOpened(true) }}>
+                  Register
+                </Button>
+              </>
+            }
+
           </Group>
         </AppShell.Header>
         <AppShell.Navbar>
@@ -66,104 +117,25 @@ const Navigation = (props: AppProps): JSX.Element => {
               onClick={() => { setActivePage('/allgames') }}
               active={activePage === '/allgames'}
             />
-            <NavLink label="Nintendo">
-              {navData?.filter(x => x.consoleType === 0).sort((a, b) => a.consoleName.localeCompare(b.consoleName)).map(data => {
-                return (
-                  <NavLink
-                    component={Link}
-                    description={`${data.gameCount} games`}
-                    key={data.consoleId}
-                    label={data.consoleName}
-                    href={`console/${data.consoleId}`}
-                    onClick={() => { setActivePage(data.consoleId.toString()) }}
-                  />
-                )
-              })}
-            </NavLink>
-            <NavLink label="Sony">
-              {navData?.filter(x => x.consoleType === 1).sort((a, b) => a.consoleName.localeCompare(b.consoleName)).map(data => {
-                return (
-                  <NavLink
-                    component={Link}
-                    description={`${data.gameCount} games`}
-                    key={data.consoleId}
-                    label={data.consoleName}
-                    href={`console/${data.consoleId}`}
-                    onClick={() => { setActivePage(data.consoleId.toString()) }}
-                  />
-                )
-              })}
-            </NavLink>
-            <NavLink label="Atari">
-              {navData?.filter(x => x.consoleType === 2).sort((a, b) => a.consoleName.localeCompare(b.consoleName)).map(data => {
-                return (
-                  <NavLink
-                    component={Link}
-                    description={`${data.gameCount} games`}
-                    key={data.consoleId}
-                    label={data.consoleName}
-                    href={`console/${data.consoleId}`}
-                    onClick={() => { setActivePage(data.consoleId.toString()) }}
-                  />
-                )
-              })}
-            </NavLink>
-            <NavLink label="Sega">
-              {navData?.filter(x => x.consoleType === 3).sort((a, b) => a.consoleName.localeCompare(b.consoleName)).map(data => {
-                return (
-                  <NavLink
-                    component={Link}
-                    description={`${data.gameCount} games`}
-                    key={data.consoleId}
-                    label={data.consoleName}
-                    href={`console/${data.consoleId}`}
-                    onClick={() => { setActivePage(data.consoleId.toString()) }}
-                  />
-                )
-              })}
-            </NavLink>
-            <NavLink label="NEC">
-              {navData?.filter(x => x.consoleType === 4).sort((a, b) => a.consoleName.localeCompare(b.consoleName)).map(data => {
-                return (
-                  <NavLink
-                    component={Link}
-                    description={`${data.gameCount} games`}
-                    key={data.consoleId}
-                    label={data.consoleName}
-                    href={`console/${data.consoleId}`}
-                    onClick={() => { setActivePage(data.consoleId.toString()) }}
-                  />
-                )
-              })}
-            </NavLink>
-            <NavLink label="SNK">
-              {navData?.filter(x => x.consoleType === 5).sort((a, b) => a.consoleName.localeCompare(b.consoleName)).map(data => {
-                return (
-                  <NavLink
-                    component={Link}
-                    description={`${data.gameCount} games`}
-                    key={data.consoleId}
-                    label={data.consoleName}
-                    href={`console/${data.consoleId}`}
-                    onClick={() => { setActivePage(data.consoleId.toString()) }}
-                  />
-                )
-              })}
-            </NavLink>
-            <NavLink label="Other">
-              {navData?.filter(x => x.consoleType === 6).sort((a, b) => a.consoleName.localeCompare(b.consoleName)).map(data => {
-                return (
-                  <NavLink
-                    component={Link}
-                    description={`${data.gameCount} games`}
-                    key={data.consoleId}
-                    label={data.consoleName}
-                    href={`console/${data.consoleId}`}
-                    onClick={() => { setActivePage(data.consoleId.toString()) }}
-                  />
-                )
-              })}
-            </NavLink>
+
+            {consoleTypes.map(console => {
+              return (
+                <NavLink label={console.consoleName} key={console.consoleType}>
+                {navData?.filter(x => x.consoleType === console.consoleType).sort((a, b) => a.consoleName.localeCompare(b.consoleName)).map(data => {
+                  return (
+                    <NavLink
+                      component={Link}
+                      description={`${data.gameCount} games`}
+                      key={data.consoleId}
+                      label={data.consoleName}
+                      href={`console/${data.consoleId}`}
+                      onClick={() => { setActivePage(data.consoleId.toString()) }}
+                    />
+                  )
+                })}
+              </NavLink>
+              )
+            })}
           </ScrollArea>
         </AppShell.Navbar>
         <AppShell.Main>
