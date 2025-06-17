04dcd522230f51ebc961e4b2c5d0cbbd7990d223
Benjamin Regan <35966588+Bregann@users.noreply.github.com>
2025-06-17 21:56:28 +0100

    search wip

--- retrotrack.web/components/pages/AllGamesComponent.tsx
+++ retrotrack.web/components/pages/AllGamesComponent.tsx
@@ -1,12 +1,13 @@
 'use client'
 import { useState, useMemo } from 'react'
-import { Container, Paper } from '@mantine/core'
+import { Button, Container, Group, Input, Paper, Select } from '@mantine/core'
 import PaginatedTable, { Column, SortOption } from '../shared/PaginatedTable'
 import { PublicGameTableColumns } from '@/interfaces/Pages/PublicGameTableColumns'
 import Image from 'next/image'
 import styles from '@/css/components/publicGamesTable.module.scss'
 import { usePublicPaginatedTableQuery } from '@/hooks/Consoles/usePublicPaginatedTableQuery'
 import type { Game, GetGamesForConsoleResponse } from '@/interfaces/Api/Games/GetGamesForConsoleResponse'
+import { useDebouncedState } from '@mantine/hooks'
 
 interface AllPagesComponentProps {
   pageData: GetGamesForConsoleResponse
@@ -57,6 +58,10 @@ export default function AllPagesComponent(props: AllPagesComponentProps) {
     direction: 'asc',
   })
 
+  const [searchInput, setSearchInput] = useDebouncedState<string | null>(null, 200)
+  const [searchTerm, setSearchTerm] = useState<string | null>(null)
+  const [searchDropdownValue, setSearchDropdownValue] = useState<string>('0')
+
   const queryString = useMemo(() => {
     const skip = (page - 1) * 100
     const take = 100
@@ -68,16 +73,26 @@ export default function AllPagesComponent(props: AllPagesComponentProps) {
     }
     const sortParam = sortKeyMap[sortOption.key] || 'SortByName'
     const sortValue = sortOption.direction === 'asc'
-    return `ConsoleId=-1&Skip=${skip}&Take=${take}&${sortParam}=${sortValue}`
-  }, [page, sortOption])
+    let query = `ConsoleId=-1&Skip=${skip}&Take=${take}&${sortParam}=${sortValue}`
+    if (searchTerm !== null && searchTerm !== '') {
+      query += `&SearchType=${searchDropdownValue}&SearchTerm=${encodeURIComponent(searchTerm)}`
+    }
+    return query
+  }, [page, searchDropdownValue, searchTerm, sortOption.direction, sortOption.key])
 
   const isFirstLoad = queryString === 'ConsoleId=-1&Skip=0&Take=100&SortByName=true'
-
 
   const { data, isLoading, isError, error } = usePublicPaginatedTableQuery(
     queryString,
     isFirstLoad ? props.pageData : undefined
   )
+
+  const searchDropdownOptions = [
+    { value: '0', label: 'Game Title' },
+    { value: '1', label: 'Genre' },
+    { value: '2', label: 'Achievement Name' },
+    { value: '3', label: 'Achievement Description' }
+  ]
 
   const totalPages = data?.totalPages ?? 0
 
@@ -88,6 +103,39 @@ export default function AllPagesComponent(props: AllPagesComponentProps) {
     <Container size="95%">
       <h1>All Games</h1>
       <Paper className={styles.paper}>
+        <Group
+          justify="center"
+          mt={10}
+          mb={10}
+          pr={20}
+          pl={20}
+          style={{ width: '100%' }}
+        >
+          <Input
+            placeholder="Search"
+            style={{
+              flex: 1,
+              minWidth: 0,
+            }}
+            onChange={(e) => setSearchInput(e.currentTarget.value)}
+          />
+          <Select
+            data={searchDropdownOptions}
+            style={{
+              flex: '0 0 150px',
+              minWidth: 0,
+            }}
+            clearable
+            defaultValue={searchDropdownValue}
+          />
+          <Button style={{ flex: '0 0 auto', ml: 10 }}
+            onClick={() => {setSearchTerm(searchInput)}}
+            disabled={!searchInput || searchInput.trim() === ''}
+          >
+    Search
+          </Button>
+        </Group>
+
         <PaginatedTable
           data={data?.games ?? []}
           columns={columns}
@@ -103,7 +151,6 @@ export default function AllPagesComponent(props: AllPagesComponentProps) {
           }}
         />
       </Paper>
-
     </Container>
   )
 }
