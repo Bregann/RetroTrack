8e0f119992543d4d6002231405698638041751a9
Benjamin Regan <35966588+Bregann@users.noreply.github.com>
2025-08-25 09:32:26 +0100

    in progress games move to react query

--- retrotrack.web/app/inprogressgames/page.tsx
+++ retrotrack.web/app/inprogressgames/page.tsx
@@ -1,7 +1,8 @@
 import InProgressGamesTable from '@/components/pages/InProgressGamesTable'
-import { doGet } from '@/helpers/apiClient'
+import { doQueryGet } from '@/helpers/apiClient'
 import { GetUserProgressForConsoleResponse } from '@/interfaces/api/games/GetUserProgressForConsoleResponse'
 import { Container, Title, Button, Text } from '@mantine/core'
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
 import { Metadata } from 'next'
 import { cookies } from 'next/headers'
 import Link from 'next/link'
@@ -16,8 +17,7 @@ export const metadata: Metadata = {
 
 export default async function Page() {
   const cookieStore = await cookies()
-
-  let loggedInData: GetUserProgressForConsoleResponse | null = null
+  const queryClient = new QueryClient()
 
   // Check if the user is logged in by checking for the accessToken cookie
   if (cookieStore.has('accessToken')) {
@@ -29,31 +29,25 @@ export default async function Page() {
       .map(c => `${c.name}=${c.value}`)
       .join('; ')
 
-    const result = await doGet<GetUserProgressForConsoleResponse>('/api/games/GetUserProgressForConsole?ConsoleId=-1&Skip=0&Take=100&SortByName=true&HideUnstartedGames=true&HideCompletedGames=true', { next: { revalidate: 60 }, cookieHeader })
-    if (result.status !== 200 || result.data === undefined) {
-      console.error('Failed to load logged in navigation data:', result.status)
-      loggedInData = null
-    } else {
-      loggedInData = result.data
-    }
+    queryClient.prefetchQuery({
+      queryKey: ['ConsoleId=-1&Skip=0&Take=100&SortByName=true&HideUnstartedGames=true&HideCompletedGames=true'],
+      queryFn: async () => await doQueryGet<GetUserProgressForConsoleResponse>('/api/games/GetUserProgressForConsole?ConsoleId=-1&Skip=0&Take=100&SortByName=true&HideUnstartedGames=true&HideCompletedGames=true', { next: { revalidate: 60 }, cookieHeader }),
+      staleTime: 60000
+    })
   }
 
   return (
     <main>
-      {loggedInData === null &&
+      {cookieStore.has('accessToken') === false &&
         <Container ta="center">
           <Title order={2} pt="xl">Error</Title>
           <Text pb="lg">Sorry about that, we couldn&apos;t find your games, either you are not logged in or an error occurred.</Text>
           <Link href="/home"><Button size="md" radius="md" variant="light">Head Home</Button></Link>
         </Container>
       }
-
-      {loggedInData !== null &&
-        <InProgressGamesTable
-          totalGames={loggedInData.totalCount}
-          pageData={loggedInData}
-        />
-      }
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <InProgressGamesTable />
+      </HydrationBoundary>
     </main>
   )
 }
--- retrotrack.web/components/pages/InProgressGamesTable.tsx
+++ retrotrack.web/components/pages/InProgressGamesTable.tsx
@@ -2,28 +2,25 @@
 'use client'
 
 import { useState, useMemo } from 'react'
-import { Badge, Button, Center, Checkbox, Container, Group, Input, Loader, Paper, Select, Text } from '@mantine/core'
+import { Badge, Button, Center, Checkbox, Container, Group, Input, Loader, Paper, Select, Text, Title } from '@mantine/core'
 import PaginatedTable, { Column, SortOption } from '../shared/PaginatedTable'
 import Image from 'next/image'
 import styles from '@/css/components/publicGamesTable.module.scss'
-import { useLoggedInPaginatedTableQuery } from '@/hooks/consoles/useLoggedInPaginatedTableQuery'
 import type { LoggedInGame, GetUserProgressForConsoleResponse } from '@/interfaces/api/games/GetUserProgressForConsoleResponse'
 import { useDebouncedState } from '@mantine/hooks'
 import { useGameModal } from '@/context/gameModalContext'
 import { HighestAwardKind } from '@/enums/highestAwardKind'
 import { Press_Start_2P } from 'next/font/google'
+import { useQuery } from '@tanstack/react-query'
+import { doQueryGet } from '@/helpers/apiClient'
+import Loading from '@/app/loading'
+import Link from 'next/link'
 
 const pressStart2P = Press_Start_2P({
   weight: '400',
   subsets: ['latin'],
   display: 'swap',
 })
-
-
-interface InProgressGamesTableProps {
-  pageData: GetUserProgressForConsoleResponse
-  totalGames: number
-}
 
 const columns: Column<LoggedInGame>[] = [
   {
@@ -106,7 +103,7 @@ const columns: Column<LoggedInGame>[] = [
   }
 ]
 
-export default function InProgressGamesTable(props: InProgressGamesTableProps) {
+export default function InProgressGamesTable() {
   const [page, setPage] = useState(1)
   const [sortOption, setSortOption] = useState<SortOption<LoggedInGame>>({
     key: 'gameTitle',
@@ -144,12 +141,11 @@ export default function InProgressGamesTable(props: InProgressGamesTableProps) {
     return query
   }, [hideBeatenGames, page, searchDropdownValue, searchTerm, sortOption.direction, sortOption.key])
 
-  const isFirstLoad = queryString === 'ConsoleId=-1&Skip=0&Take=100&SortByName=true&HideUnstartedGames=true&HideCompletedGames=true'
-
-  const { data, isLoading, isError, error } = useLoggedInPaginatedTableQuery(
-    queryString,
-    isFirstLoad ? props.pageData : undefined
-  )
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: [queryString],
+    queryFn: async () => await doQueryGet<GetUserProgressForConsoleResponse>(`/api/games/GetUserProgressForConsole?${queryString}`, { next: { revalidate: 60 } }),
+    staleTime: 60000,
+  })
 
   const gameModal = useGameModal()
 
@@ -164,88 +160,102 @@ export default function InProgressGamesTable(props: InProgressGamesTableProps) {
 
   return (
     <Container size="95%">
-      <Container ta="center" py="xs">
-        <Text
-          size={'28px'}
-          mt={'md'}
-          ta="center"
-          className={pressStart2P.className}
-        >In Progress Games</Text>
-        <Text mb="xs">There are a total of {props.totalGames} games!</Text>
-      </Container>
+      {isLoading &&
+        <Loading />
+      }
+      {isError &&
+        <Container ta="center">
+          <Title order={2} pt="xl">Error</Title>
+          <Text pb="lg">Sorry about that, we couldn&apos;t load the game data, try again later.</Text>
+          <Button size="md" radius="md" variant="light" component={Link} href={'/home'}>Head Home</Button>
+        </Container>
+      }
+      {data !== undefined &&
+        <>
+          <Container ta="center" py="xs">
+            <Text
+              size={'28px'}
+              mt={'md'}
+              ta="center"
+              className={pressStart2P.className}
+            >In Progress Games</Text>
+            <Text mb="xs">There are a total of {data.totalCount} games!</Text>
+          </Container>
 
-      <Paper className={styles.paper}>
-        {isLoading ? (
-          <Center style={{ padding: '2rem' }}>
-            <Loader variant="dots" size="lg" />
-            <p style={{ marginLeft: 10 }}>Loading games...</p>
-          </Center>
-        ) : isError ? (
-          <p style={{ color: 'red' }}>Oops: {error.message}</p>
-        ) : (
-          <>
-            <Group
-              justify="center"
-              mt={10}
-              mb={10}
-              pr={20}
-              pl={20}
-              style={{ width: '100%' }}
-            >
-              <Input
-                placeholder="Search..."
-                style={{
-                  flex: 1,
-                  minWidth: 0,
-                }}
-                onChange={(e) => {
-                  if (e.currentTarget.value.trim() === '') {
-                    setSearchTerm(null)
-                    setSearchInput(null)
-                  }
-                  setSearchInput(e.currentTarget.value)
-                }
-                }
-              />
-              <Select
-                data={searchDropdownOptions}
-                style={{
-                  flex: '0 0 150px',
-                  minWidth: 0,
-                }}
-                clearable
-                defaultValue={searchDropdownValue}
-                onChange={(value) => setSearchDropdownValue(value ?? '0')}
-              />
-              <Button style={{ flex: '0 0 auto', ml: 10 }}
-                onClick={() => { setSearchTerm(searchInput) }}
-                disabled={searchInput === null || searchInput.trim() === ''}
-              >
-                Search
-              </Button>
-            </Group>
-            <Group ml={20}>
-              <Checkbox checked={hideBeatenGames} label="Hide Beaten Games" onChange={() => { setHideBeatenGames(!hideBeatenGames) }} />
-            </Group>
-            <PaginatedTable
-              data={data!.games}
-              columns={columns}
-              page={page}
-              total={totalPages}
-              sortOption={sortOption}
-              onSortChange={(opt) => {
-                setPage(1)
-                setSortOption(opt)
-              }}
-              onPageChange={setPage}
-              onRowClick={(item) => {
-                gameModal.showModal(item.gameId)
-              }}
-            />
-          </>
-
-        )}
-      </Paper>
+          <Paper className={styles.paper}>
+            {isLoading ? (
+              <Center style={{ padding: '2rem' }}>
+                <Loader variant="dots" size="lg" />
+                <p style={{ marginLeft: 10 }}>Loading games...</p>
+              </Center>
+            ) : isError ? (
+              <p style={{ color: 'red' }}>Oops: {error.message}</p>
+            ) : (
+              <>
+                <Group
+                  justify="center"
+                  mt={10}
+                  mb={10}
+                  pr={20}
+                  pl={20}
+                  style={{ width: '100%' }}
+                >
+                  <Input
+                    placeholder="Search..."
+                    style={{
+                      flex: 1,
+                      minWidth: 0,
+                    }}
+                    onChange={(e) => {
+                      if (e.currentTarget.value.trim() === '') {
+                        setSearchTerm(null)
+                        setSearchInput(null)
+                      }
+                      setSearchInput(e.currentTarget.value)
+                    }
+                    }
+                  />
+                  <Select
+                    data={searchDropdownOptions}
+                    style={{
+                      flex: '0 0 150px',
+                      minWidth: 0,
+                    }}
+                    clearable
+                    defaultValue={searchDropdownValue}
+                    onChange={(value) => setSearchDropdownValue(value ?? '0')}
+                  />
+                  <Button style={{ flex: '0 0 auto', ml: 10 }}
+                    onClick={() => { setSearchTerm(searchInput) }}
+                    disabled={searchInput === null || searchInput.trim() === ''}
+                  >
+                    Search
+                  </Button>
+                </Group>
+                <Group ml={20}>
+                  <Checkbox checked={hideBeatenGames} label="Hide Beaten Games" onChange={() => { setHideBeatenGames(!hideBeatenGames) }} />
+                </Group>
+                <PaginatedTable
+                  data={data!.games}
+                  columns={columns}
+                  page={page}
+                  total={totalPages}
+                  sortOption={sortOption}
+                  onSortChange={(opt) => {
+                    setPage(1)
+                    setSortOption(opt)
+                  }}
+                  onPageChange={setPage}
+                  onRowClick={(item) => {
+                    gameModal.showModal(item.gameId)
+                  }}
+                />
+              </>
+            )}
+          </Paper>
+        </>
+      }
     </Container>
+
   )
 }
