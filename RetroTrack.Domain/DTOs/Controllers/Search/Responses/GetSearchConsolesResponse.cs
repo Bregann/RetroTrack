160a47f338102aa9cb9d96daaf2d8d5ed5478294
Benjamin Regan <35966588+Bregann@users.noreply.github.com>
2025-09-05 19:59:12 +0100

    code cleanup

--- RetroTrack.Domain/DTOs/Controllers/Search/Responses/GetSearchConsolesResponse.cs
+++ RetroTrack.Domain/DTOs/Controllers/Search/Responses/GetSearchConsolesResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RetroTrack.Domain.DTOs.Controllers.Search.Responses
+﻿namespace RetroTrack.Domain.DTOs.Controllers.Search.Responses
 {
     public class GetSearchConsolesResponse
     {
--- RetroTrack.Domain/Interfaces/Controllers/ISearchControllerDataService.cs
+++ RetroTrack.Domain/Interfaces/Controllers/ISearchControllerDataService.cs
@@ -1,10 +1,5 @@
 ﻿using RetroTrack.Domain.DTOs.Controllers.Search.Requests;
 using RetroTrack.Domain.DTOs.Controllers.Search.Responses;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RetroTrack.Domain.Interfaces.Controllers
 {
